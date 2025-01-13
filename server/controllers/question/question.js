c73b43a424c525cfb9937de6b2fac00d36b53277
Uzkhu <uzkhuthef@gmail.com>
2025-01-13 16:24:20 +0800

    test

--- server/controllers/question/question.js
+++ server/controllers/question/question.js
@@ -1,11 +1,9 @@
-import { sql } from "../../database/index.js"; // Import the SQL database connection
+import { sql } from "../../database/index.js";
 
 export const Question = async (req, res) => {
   try {
-    // Fetch questions from the database
     const questions = await sql`SELECT * FROM questions`;
 
-    // Check if there are any questions
     if (questions.length === 0) {
       return res.status(404).send({ message: "No questions found." });
     }
--- server/controllers/users/addquestion.js
+++ server/controllers/users/addquestion.js
@@ -1,16 +1,13 @@
-import { sql } from "../../database/index.js"; // Import the SQL database connection
+import { sql } from "../../database/index.js";
 
 export const AddQuestion = async (req, res) => {
-  const { question, answer } = req.body; // Extract question and answer from the request body
-
+  const { question, answer } = req.body;
   try {
-    // Insert the new question into the questions table
     const [newQuestion] = await sql`
       INSERT INTO questions (question, answer)
       VALUES (${question}, ${answer})
       RETURNING *`;
 
-    // Return the newly added question
     res.status(201).json(newQuestion);
   } catch (error) {
     console.error("Error inserting the question into the database:", error);
